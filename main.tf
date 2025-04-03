42603591e4afe7238eac3742a7c19ba0bae9c8fb
Tomasz Grabusiński <tomasz.grabusinski@xebia.com>
2025-04-04 00:37:21 +0200

    add terraform files

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "spacelift-example-rg"
+  location = "East US"
+}
