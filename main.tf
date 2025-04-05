94da9e9814ca834a000b68645e82dcd30fed3060
Tomasz Grabusiński <tomasz.grabusinski@xebia.com>
2025-04-05 12:04:30 +0200

    add second Azure resource group configuration

--- main.tf
+++ main.tf
@@ -16,3 +16,8 @@ resource "azurerm_resource_group" "example" {
   name     = "spacelift-example-rg"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = "spacelift-example2-rg"
+  location = "East US"
+}
